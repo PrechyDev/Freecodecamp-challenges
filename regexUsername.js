7bbf6db980e600b2a2d66e861450cbf72bc0bc13
Precious Okafor <preciousrobinsonokafor@gmail.com>
2021-02-06 19:45:01 +0100

    regex for checking usernames

---
+++ regexUsername.js
@@ -0,0 +1,19 @@
+/*Usernames can only use alpha-numeric characters.
+
+The only numbers in the username have to be at the end. There can be zero or more of them at the end. Username cannot start with the number.
+
+Username letters can be lowercase and uppercase.
+
+Usernames have to be at least two characters long. A two-character username can only use alphabet letters as characters.
+*/
+
+let username = "JackOfAllTrades";
+let userCheck = /^[a-z][a-z]+\d*$|^[a-z]\d\d+$/i; 
+let result = userCheck.test(username);
+
+/*
+^[a-z][a-z]+\d*$ - starts with an alphabet, the next chars are alphabets, ends with 0 or more digits
+(if its just 2 chars, they have to be alphabets).
+
+^[a-z]\d\d+$ - starts with an alphabet, ends with two or more digits (username has to be 3 or more chars).
+*/
